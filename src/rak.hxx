65f86d53322dbb6ecfd2c8294a06b2d71f3d1cfe
Subhajit Sahu <wolfram77@gmail.com>
2022-11-03 19:20:22 +0530

    :bug: w/ rakSeqStatic()

--- src/rak.hxx
+++ src/rak.hxx
@@ -1,9 +1,11 @@
 #pragma once
-#include <vector>
 #include <utility>
+#include <vector>
 #include "_main.hxx"
 
+using std::pair;
 using std::vector;
+using std::make_pair;
 using std::move;
 
 
@@ -13,11 +15,12 @@ using std::move;
 // -----------
 
 struct RakOptions {
-  int  repeat;
-  int  maxIterations;
+  int   repeat;
+  float tolerance;
+  int   maxIterations;
 
-  RakOptions(int repeat=1, int maxIterations=500) :
-  repeat(repeat), maxIterations(maxIterations) {}
+  RakOptions(int repeat=1, float tolerance=0.05, int maxIterations=500) :
+  repeat(repeat), tolerance(tolerance), maxIterations(maxIterations) {}
 };
 
 
@@ -38,3 +41,91 @@ struct RakResult {
   RakResult(vector<K>& membership, int iterations=0, float time=0) :
   membership(move(membership)), iterations(iterations), time(time) {}
 };
+
+
+
+
+// RAK-INITIALIZE
+// --------------
+
+/**
+ * Initialize communities such that each vertex is its own community.
+ * @param vcom community each vertex belongs to (updated)
+ * @param x original graph
+ */
+template <class G, class K>
+inline void rakInitialize(vector<K>& vcom, const G& x) {
+  x.forEachVertexKey([&](auto u) { vcom[u] = u; });
+}
+
+
+
+
+// RAK-CHOOSE-COMMUNITY
+// --------------------
+
+/**
+ * Scan an edge community connected to a vertex.
+ * @param vcs communities vertex u is linked to (updated)
+ * @param vcout total edge weight from vertex u to community C (updated)
+ * @param u given vertex
+ * @param v outgoing edge vertex
+ * @param w outgoing edge weight
+ * @param vcom community each vertex belongs to
+ */
+template <bool SELF=false, class K, class V>
+inline void rakScanCommunity(vector<K>& vcs, vector<V>& vcout, K u, K v, V w, const vector<K>& vcom) {
+  if (!SELF && u==v) return;
+  K c = vcom[v];
+  if (!vcout[c]) vcs.push_back(c);
+  vcout[c] += w;
+}
+
+
+/**
+ * Scan communities connected to a vertex.
+ * @param vcs communities vertex u is linked to (updated)
+ * @param vcout total edge weight from vertex u to community C (updated)
+ * @param x original graph
+ * @param u given vertex
+ * @param vcom community each vertex belongs to
+ */
+template <bool SELF=false, class G, class K, class V>
+inline void rakScanCommunities(vector<K>& vcs, vector<V>& vcout, const G& x, K u, const vector<K>& vcom) {
+  x.forEachEdge(u, [&](auto v, auto w) { rakScanCommunity<SELF>(vcs, vcout, u, v, w, vcom); });
+}
+
+
+/**
+ * Clear communities scan data.
+ * @param vcs total edge weight from vertex u to community C (updated)
+ * @param vcout communities vertex u is linked to (updated)
+ */
+template <class K, class V>
+inline void rakClearScan(vector<K>& vcs, vector<V>& vcout) {
+  for (K c : vcs)
+    vcout[c] = V();
+  vcs.clear();
+}
+
+
+/**
+ * Choose connected community with most weight.
+ * @param x original graph
+ * @param u given vertex
+ * @param vcom community each vertex belongs to
+ * @param vcs communities vertex u is linked to
+ * @param vcout total edge weight from vertex u to community C
+ * @returns [best community, best edge weight to community]
+ */
+template <bool SELF=false, class G, class K, class V>
+inline pair<K, V> rakChooseCommunity(const G& x, K u, const vector<K>& vcom, const vector<K>& vcs, const vector<V>& vcout) {
+  K d = vcom[u];
+  K cmax = K();
+  V wmax = V();
+  for (K c : vcs) {
+    if (!SELF && c==d) continue;
+    if (vcout[c]>wmax) { cmax = c; wmax = vcout[c]; }
+  }
+  return make_pair(cmax, wmax);
+}
--- src/rakSeq.hxx
+++ src/rakSeq.hxx
@@ -5,7 +5,7 @@
 #include "vertices.hxx"
 #include "edges.hxx"
 #include "csr.hxx"
-#include "pagerank.hxx"
+#include "rak.hxx"
 
 using std::vector;
 using std::swap;
@@ -13,87 +13,62 @@ using std::swap;
 
 
 
-// PAGERANK-FACTOR
-// ---------------
-// For contribution factors of vertices (unchanging).
+// RAK-MOVE-ITERATION
+// ------------------
 
-template <class T>
-void pagerankFactorW(vector<T>& a, const vector<int>& vdata, int i, int n, T p) {
-  for (int u=i; u<i+n; u++) {
-    int d = vdata[u];
-    a[u] = d>0? p/d : 0;
-  }
-}
-
-
-
-
-// PAGERANK-TELEPORT
-// -----------------
-// For teleport contribution from vertices (inc. dead ends).
-
-template <class T>
-T pagerankTeleport(const vector<T>& r, const vector<int>& vdata, int N, T p) {
-  T a = (1-p)/N;
-  for (int u=0; u<N; u++)
-    if (vdata[u] == 0) a += p*r[u]/N;
+/**
+ * Move each vertex to its best community.
+ * @param vcs communities vertex u is linked to (updated)
+ * @param vcout total edge weight from vertex u to community C (updated)
+ * @param vcom community each vertex belongs to (updated)
+ * @param x original graph
+ * @param vdom community each vertex belonged to
+ * @returns number of changed vertices
+ */
+template <class G, class K, class V>
+K rakMoveIteration(vector<K>& vcs, vector<V>& vcout, vector<K>& vcom, const G& x, const vector<K>& vdom) {
+  K a = K();
+  x.forEachVertexKey([&](auto u) {
+    rakClearScan(vcs, vcout);
+    rakScanCommunities(vcs, vcout, x, u, vdom);
+    auto [c, w] = rakChooseCommunity(x, u, vdom, vcs, vcout);
+    if (c) { vcom[u] = c; ++a; }
+  });
   return a;
 }
 
 
 
 
-// PAGERANK-CALCULATE
-// ------------------
-// For rank calculation from in-edges.
+// RAK-SEQ
+// -------
 
-template <class T>
-void pagerankCalculateW(vector<T>& a, const vector<T>& c, const vector<int>& vfrom, const vector<int>& efrom, int i, int n, T c0) {
-  for (int v=i; v<i+n; v++)
-    a[v] = c0 + sumValuesAt(c, sliceIterable(efrom, vfrom[v], vfrom[v+1]));
-}
-
-
-
-
-// PAGERANK-ERROR
-// --------------
-// For convergence check.
-
-template <class T>
-T pagerankError(const vector<T>& x, const vector<T>& y, int i, int N, int EF) {
-  switch (EF) {
-    case 1:  return l1Norm(x, y, i, N);
-    case 2:  return l2Norm(x, y, i, N);
-    default: return liNorm(x, y, i, N);
-  }
-}
-
-
-
-
-// PAGERANK
-// --------
-// For Monolithic / Componentwise PageRank.
-
-template <class H, class J, class M, class FL, class T=float>
-PagerankResult<T> pagerankSeq(const H& xt, const J& ks, int i, const M& ns, FL fl, const vector<T> *q, const PagerankOptions<T>& o) {
-  int  N  = xt.order();
-  T    p  = o.damping;
-  T    E  = o.tolerance;
-  int  L  = o.maxIterations, l = 0;
-  int  EF = o.toleranceNorm;
-  auto vfrom = sourceOffsetsAs(xt, ks, int());
-  auto efrom = destinationIndicesAs(xt, ks, int());
-  auto vdata = vertexData(xt, ks);
-  vector<T> a(N), r(N), c(N), f(N), qc;
-  if (q) qc = compressContainer(xt, *q, ks);
-  float t = measureDurationMarked([&](auto mark) {
-    if (q) copyValuesW(r, qc);   // copy old ranks (qc), if given
-    else fillValueU(r, T(1)/N);
-    copyValuesW(a, r);
-    mark([&] { pagerankFactorW(f, vdata, 0, N, p); multiplyValuesW(c, a, f, 0, N); });  // calculate factors (f) and contributions (c)
-    mark([&] { l = fl(a, r, c, f, vfrom, efrom, vdata, i, ns, N, p, E, L, EF); });      // calculate ranks of vertices
+template <bool ASYNC=false, class G, class K>
+RakResult<K> rakSeq(const G& x, const vector<K>* q, const RakOptions& o) {
+  using V = typename G::edge_value_type;
+  int l = 0;
+  K S = x.span();
+  K N = x.order();
+  vector<K> vdom(S), vcom(S), vcs;
+  vector<V> vcout(S);
+  float t = measureDuration([&]() {
+    rakInitialize(vdom, x);
+    for (l=0; l<o.maxIterations;) {
+      K n = rakMoveIteration(vcs, vcout, !ASYNC? vcom : vdom, x, vdom); ++l;
+      if (float(n)/N <= o.tolerance) break;
+      if (!ASYNC) swap(vcom, vdom);
+    }
   }, o.repeat);
-  return {decompressContainer(xt, a, ks), l, t};
+  return {vdom, l, t};
+}
+
+
+
+
+// RAK-SEQ-STATIC
+// --------------
+
+template <bool ASYNC=false, class G, class K>
+inline auto rakSeqStatic(const G& x, const vector<K>* q=nullptr, const RakOptions& o={}) {
+  return rakSeq<ASYNC>(x, q, o);
 }
